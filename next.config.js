74f762b3c460ff8d3b2b4649f10be117ebee4195
two6ean <twobean125@gmail.com>
2026-06-10 15:18:06 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 const nextConfig = {
-<<<<<<< HEAD
-=======
-  distDir: process.platform === 'win32' ? '.next-win' : '.next-wsl',
-  output: 'export',
->>>>>>> d3bba130088f5f423c05114f3138edda4141b675
   images: {
     unoptimized: true,
   },
